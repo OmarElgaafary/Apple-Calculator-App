5b80878e256b288f30b6ab0d5408ed28ba546e18
Omar Adly <omarelgaafaryy@gmail.com>
2025-07-08 18:35:30 +0300

    Changed calculator mechanisms.

--- calculator.js
+++ calculator.js
@@ -2,11 +2,8 @@ const calculatorOuterContainer = document.querySelector('.calculator-outer-conta
 const calculatorButtons = document.querySelectorAll('.calculator-button');
 const calculatorResultElement = document.querySelector('.calculator-result');
 
-let firstNumber = '';
-let secondNumber = '';
-let operatorApplied = false;
-let operatorButton;
-let resultNum;
+let calcNums = '';
+let calcResult = '';
 
 calculatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -20,108 +17,121 @@ function handleCalculation(button) {
     // if its finished by applying an operator start applying to the secod number
     // if the user presses equal, calculate result and display it
 
-    switch (button) {
+
+
+    // check if incoming input is an integer is it is append it to the displayNumbers array
+    // if more numbers are pressed they are still pushed to form a bigger number whilst updating that html
+
+    //if an operator is inputed push it to the array and update it to the html
+
+    //if the equals sign is pressed, calculate the total of array using eval() func
+
+    // edge cases:
+
+    // if you are inputing an equation and get the result of it, you can use that result as the starting value of another operation
+
+    // if you calculate the value of an equation and you press another number, it should cancel the equation and start a new one
+
+    let buttonValue = String(button);
+
+    switch (buttonValue) {
         case '/':
-            if (!firstNumber && resultNum)
-            {
-                firstNumber = String(resultNum);
+            if (calcResult !== '') {
+                calcNums = calcResult;
             }
-            operatorButton = '/';
-            operatorApplied = true;
+            if (checkLastIndex) {
+                calcNums += '/';
+                calcDisplay();
+            }
+            console.log(calcNums);
             return;
         case '*':
-            if (!firstNumber && resultNum)
-            {
-                firstNumber = String(resultNum);
+            if (calcResult !== '') {
+                calcNums = calcResult;
             }
-            operatorButton = '*';
-            operatorApplied = true;
+
+            if (checkLastIndex()) {
+                calcNums += '*';
+                calcDisplay();
+            }
             return;
         case '-':
-            if (!firstNumber && resultNum)
-            {
-                firstNumber = String(resultNum);
+            if (calcResult !== '') {
+                calcNums = calcResult;
             }
-            operatorButton = '-';
-            operatorApplied = true;
+
+            if (checkLastIndex()) {
+                calcNums += '-';
+                calcDisplay();
+            }
+            console.log(calcNums);
             return;
         case '+':
-            if (!firstNumber && resultNum)
-            {
-                firstNumber = String(resultNum);
+            if (calcResult !== '') {
+                calcNums = Number(calcResult);
             }
-            operatorButton = '+';
-            operatorApplied = true;
+            if (checkLastIndex()) {
+                calcNums += '+';
+                calcDisplay();
+            }
+            console.log(calcNums);
             return;
         case '=':
-            if (operatorButton == '+') {
-                resultNum = Number(firstNumber) + Number(secondNumber);
-                calculatorResultElement.innerHTML = resultNum;
-                resetValues();
-                console.log(resultNum);
+            if (checkLastIndex()) {
+                calcResult = Number(eval(calcNums));
+                calculatorResultElement.innerHTML = calcResult;
             }
-            if (operatorButton == '-') {
-                resultNum = Number(firstNumber) - Number(secondNumber);
-                calculatorResultElement.innerHTML = resultNum;
-                resetValues();
-                console.log(resultNum);
-            }
-            if (operatorButton == '*') {
-                resultNum = Number(firstNumber) * Number(secondNumber);
-                calculatorResultElement.innerHTML = resultNum;
-                resetValues();
-                console.log(resultNum);
-            }
-            if (operatorButton == '/') {
-                resultNum = Number(firstNumber) / Number(secondNumber);
-                calculatorResultElement.innerHTML = resultNum;
-                resetValues();
-                console.log(resultNum);
-            }
-            
             return;
         case 'AC':
-            calculatorResultElement.innerHTML = '';
             resetValues();
             return;
-        case 'sign':
-            if (firstNumber && !secondNumber)
-            {
-                firstNumber = -firstNumber;
-                calculatorResultElement.innerHTML = firstNumber;
-            }
-            else if (secondNumber)
-            {
-                secondNumber = -secondNumber;
-                calculatorResultElement.innerHTML = secondNumber;
-            }
-            else if (resultNum)
-            {
-                resultNum = -resultNum;
-                calculatorResultElement.innerHTML = resultNum;
-            }
-            
-            
-            return;
-        case '%':
-            
     }
 
-    if (!operatorApplied) {
-        firstNumber += button;
-        calculatorResultElement.innerHTML = firstNumber;
-    }
-    else {
-        secondNumber += button;
-        calculatorResultElement.innerHTML = secondNumber;
-    }
+    if (calcResult !== '')
+        resetValues();
 
+
+    calcNums += buttonValue;
+    calcDisplay();
+
+    console.log(calcNums);
+    console.log(calcNums.length);
+
+
+
+
+}
+
+function checkLastIndex() {
+    if (checkSigns(calcNums[(calcNums.length) - 1]))
+        return true;
+    else
+        return false;
 }
 
 function resetValues() {
-    firstNumber = '';
-    secondNumber = '';
-    operatorApplied = false;
-    operatorButton = '';
+    calculatorResultElement.innerHTML = calcResult = calcNums = '';
 }
 
+function calcDisplay() {
+    let calcFormatted = calcNums;
+    if (calcFormatted.includes('*')) {
+        console.log('found');
+        calcFormatted = calcFormatted.replaceAll('*', '×');
+    }
+
+    if (calcFormatted.includes('/')) {
+        console.log('found');
+        calcFormatted = calcFormatted.replaceAll('/', '÷');
+    }
+
+    calculatorResultElement.innerHTML = calcFormatted;
+}
+
+
+function checkSigns(sign) {
+    if (sign === '/' || sign === '*' || sign === '-' || sign === '+')
+        return false;
+    else
+        return true;
+}
